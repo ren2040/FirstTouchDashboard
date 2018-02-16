6c8e73ebf40d4b251ebff5df3ab338f71f21832a
Renji Shaji <RShaji@ad.hackney.gov.uk>
2018-02-16 10:04:46 +0000

    Commits complete

--- FirstTouchDashBoard/Controllers/CertificatesController.cs
+++ FirstTouchDashBoard/Controllers/CertificatesController.cs
@@ -15,14 +15,12 @@ namespace FirstTouchDashBoard.Controllers
         private EDOCSIntegrationHubEntities db = new EDOCSIntegrationHubEntities();
 
         // GET: Certificates
+        [HttpGet]
         public ActionResult Index(string SortOrder)
         {
 
             var mod = new ExtendedCertificates();
             mod.lCertificates = db.FirstTouchCertificates.ToList();
-
-
-          
 
             if (mod.from != null)
             {
@@ -36,17 +34,24 @@ namespace FirstTouchDashBoard.Controllers
             {
                 mod.lCertificates = mod.lCertificates.Where(m => m.postcode.Contains(m.postcode)).ToList();
             }
-            if(mod.propertyid != null)
+            if(!string.IsNullOrEmpty(mod.propertyid))
             {
                 mod.lCertificates = mod.lCertificates.Where(m => m.propertyid.Contains(m.propertyid)).ToList();
             }
-            if(mod.uprn != null)
+            if(!string.IsNullOrEmpty(mod.uprn))
             {
-                mod.lCertificates = mod.lCertificates.Where(m => m.uprn.Contains(m.uprn)).ToList();
+                    mod.lCertificates = mod.lCertificates.Where(m => m.uprn.Contains(mod.uprn)).ToList();
             }
             if(mod.certType != null)
             {
-                mod.lCertificates = mod.lCertificates.Where(m => m.certtype.Contains(m.certtype)).ToList();
+                if (mod.certType.ToLower().Equals("both"))
+                {
+
+                }
+                else
+                {
+                    mod.lCertificates = mod.lCertificates.Where(m => m.certtype.Contains(m.certtype)).ToList();
+                }
             }
 
                 ViewBag.propertyid = String.IsNullOrEmpty(SortOrder) ? "propertyid_desc" : "";
@@ -75,89 +80,212 @@ namespace FirstTouchDashBoard.Controllers
                        
                         break;
                     case "uprn_desc":
-                        emp = emp.OrderByDescending(s => s.uprn);
+                        mod.lCertificates = mod.lCertificates.AsEnumerable().OrderByDescending(s => s.uprn).ToList();
                         break;
                     case "uprn":
                         emp = emp.OrderBy(s => s.uprn);
                         break;
                     case "addressline1_desc":
-                        emp = emp.OrderByDescending(s => s.addressline1);
+                        mod.lCertificates = mod.lCertificates.AsEnumerable().OrderByDescending(s => s.addressline1).ToList();
                         break;
                     case "addressline1":
                         emp = emp.OrderBy(s => s.addressline1);
                         break;
                     case "certtype_desc":
-                        emp = emp.OrderByDescending(s => s.certtype);
+                        mod.lCertificates = mod.lCertificates.AsEnumerable().OrderByDescending(s => s.certtype).ToList();
                         break;
                     case "certtype":
-                        emp = emp.OrderBy(s => s.certtype);
+                        mod.lCertificates = mod.lCertificates.AsEnumerable().OrderBy(s => s.certtype).ToList();
                         break;
                     case "postcode":
-                        emp = emp.OrderBy(s => s.postcode);
+                        mod.lCertificates = mod.lCertificates.AsEnumerable().OrderBy(s => s.postcode).ToList();
                         break;
                     case "operativename":
-                        emp = emp.OrderBy(s => s.operativename);
+                        mod.lCertificates = mod.lCertificates.AsEnumerable().OrderBy(s => s.operativename).ToList();
                         break;
                     case "tradegroup":
-                        emp = emp.OrderBy(s => s.tradegroup);
+                        mod.lCertificates = mod.lCertificates.AsEnumerable().OrderBy(s => s.tradegroup).ToList();
                         break;
                     case "eDocsRef":
-                        emp = emp.OrderBy(s => s.eDocsRef);
+                        mod.lCertificates = mod.lCertificates.AsEnumerable().OrderBy(s => s.eDocsRef).ToList();
                         break;
                     case "status":
-                        emp = emp.OrderBy(s => s.status);
+                        mod.lCertificates = mod.lCertificates.AsEnumerable().OrderBy(s => s.status).ToList();
                         break;
                     case "datetime":
-                        emp = emp.OrderBy(s => s.datetime);
+                        mod.lCertificates = mod.lCertificates.AsEnumerable().OrderBy(s => s.datetime).ToList();
                         break;
                     case "cause":
-                        emp = emp.OrderBy(s => s.cause);
+                        mod.lCertificates = mod.lCertificates.AsEnumerable().OrderBy(s => s.cause).ToList();
                         break;
                     case "postcode_desc":
-                        emp = emp.OrderByDescending(s => s.postcode);
+                        mod.lCertificates = mod.lCertificates.AsEnumerable().OrderByDescending(s => s.postcode).ToList(); ;
                         break;
                     case "operativename_desc":
-                        emp = emp.OrderByDescending(s => s.operativename);
+                        mod.lCertificates = mod.lCertificates.AsEnumerable().OrderByDescending(s => s.operativename).ToList();
                         break;
                     case "tradegroup_desc":
-                        emp = emp.OrderByDescending(s => s.tradegroup);
+                        mod.lCertificates = mod.lCertificates.AsEnumerable().OrderByDescending(s => s.tradegroup).ToList();
                         break;
                     case "eDocsRef_desc":
-                        emp = emp.OrderByDescending(s => s.eDocsRef);
+                        mod.lCertificates = mod.lCertificates.AsEnumerable().OrderByDescending(s => s.eDocsRef).ToList();
                         break;
                     case "status_desc":
-                        emp = emp.OrderByDescending(s => s.status);
+                        mod.lCertificates = mod.lCertificates.AsEnumerable().OrderByDescending(s => s.status).ToList();
                         break;
                     case "datetime_desc":
-                        emp = emp.OrderByDescending(s => s.datetime);
+                        mod.lCertificates = mod.lCertificates.AsEnumerable().OrderByDescending(s => s.datetime).ToList();
                         break;
                     case "cause_desc":
-                        emp = emp.OrderByDescending(s => s.cause);
+                        mod.lCertificates = mod.lCertificates.AsEnumerable().OrderByDescending(s => s.cause).ToList();
                         break;
                     default:
-                        emp = emp.OrderBy(s => s.propertyid);
+                        mod.lCertificates = mod.lCertificates.AsEnumerable().OrderBy(s => s.propertyid).ToList();
                         break;
-                    
+                }
+            }
+                return View(mod);
+        }
 
+
+
+
+        // GET: Certificates
+        [HttpPost]
+        public ActionResult Index(string SortOrder, ExtendedCertificates model)
+        {
+            model.lCertificates = db.FirstTouchCertificates.ToList();
+
+            if (model.from != null)
+            {
+                model.lCertificates = model.lCertificates.Where(m => m.datetime > model.from).ToList();
+            }
+            if (model.to != null)
+            {
+                model.lCertificates = model.lCertificates.Where(m => m.datetime < model.to).ToList();
+            }
+            if (!string.IsNullOrEmpty(model.postCode))
+            {
+                model.lCertificates = model.lCertificates.Where(m => m.postcode != null).ToList();
+                model.lCertificates = model.lCertificates.Where(m => m.postcode.Contains(model.postCode)).ToList();
+            }
+            if (model.propertyid != null)
+            {
+                model.lCertificates = model.lCertificates.Where(m => m.propertyid != null).ToList();
+                model.lCertificates = model.lCertificates.Where(m => m.propertyid.Contains(model.propertyid)).ToList();
+            }
+            if (!string.IsNullOrEmpty(model.uprn))
+            {
+                model.lCertificates = model.lCertificates.Where(m => m.uprn != null).ToList();
+                model.lCertificates = model.lCertificates.Where(m => m.uprn.Contains(model.uprn)).ToList();
+            }
+            if (model.certType != null)
+            {
+                if (model.certType.ToLower().Equals("both"))
+                {
 
                 }
-                 
-             
-
+                else
+                {
+                    model.lCertificates = model.lCertificates.Where(m => m.certtype != null).ToList();
+                    model.lCertificates = model.lCertificates.Where(m => m.certtype.Contains(model.certType)).ToList();
+                }
             }
-            
+
+            ViewBag.propertyid = String.IsNullOrEmpty(SortOrder) ? "propertyid_desc" : "";
+            ViewBag.uprn = SortOrder == "uprn" ? "uprn_desc" : "uprn";
+            ViewBag.addressline1 = SortOrder == "addressline1" ? "addressline1_desc" : "addressline1";
+            ViewBag.certtype = SortOrder == "certtype" ? "certtype_desc" : "certtype";
+            ViewBag.postcode = SortOrder == "postcode" ? "postcode_desc" : "postcode";
+            ViewBag.operativename = SortOrder == "operativename" ? "operativename_desc" : "operativename";
+            ViewBag.tradegroup = SortOrder == "tradegroup" ? "tradegroup_desc" : "tradegroup";
+            ViewBag.eDocsRef = SortOrder == "eDocsRef" ? "eDocsRef_desc" : "eDocsRef";
+            ViewBag.status = SortOrder == "status" ? "status_desc" : "status";
+            ViewBag.datetime = SortOrder == "datetime" ? "datetime_desc" : "datetime";
+            ViewBag.cause = SortOrder == "cause" ? "cause_desc" : "cause";
+
+            var emp = from s in db.FirstTouchCertificates
+                      select s;
 
 
-          
-                return View(emp.ToList());
-          
-            
-              
+            if (!string.IsNullOrEmpty(SortOrder))
+            {
+                switch (SortOrder)
+                {
+                    case "propertyid_desc":
 
-            
+                        model.lCertificates = model.lCertificates.AsEnumerable().OrderByDescending(s => s.propertyid).ToList();
 
-           
+                        break;
+                    case "uprn_desc":
+                        model.lCertificates = model.lCertificates.AsEnumerable().OrderByDescending(s => s.uprn).ToList();
+                        break;
+                    case "uprn":
+                        emp = emp.OrderBy(s => s.uprn);
+                        break;
+                    case "addressline1_desc":
+                        model.lCertificates = model.lCertificates.AsEnumerable().OrderByDescending(s => s.addressline1).ToList();
+                        break;
+                    case "addressline1":
+                        emp = emp.OrderBy(s => s.addressline1);
+                        break;
+                    case "certtype_desc":
+                        model.lCertificates = model.lCertificates.AsEnumerable().OrderByDescending(s => s.certtype).ToList();
+                        break;
+                    case "certtype":
+                        model.lCertificates = model.lCertificates.AsEnumerable().OrderBy(s => s.certtype).ToList();
+                        break;
+                    case "postcode":
+                        model.lCertificates = model.lCertificates.AsEnumerable().OrderBy(s => s.postcode).ToList();
+                        break;
+                    case "operativename":
+                        model.lCertificates = model.lCertificates.AsEnumerable().OrderBy(s => s.operativename).ToList();
+                        break;
+                    case "tradegroup":
+                        model.lCertificates = model.lCertificates.AsEnumerable().OrderBy(s => s.tradegroup).ToList();
+                        break;
+                    case "eDocsRef":
+                        model.lCertificates = model.lCertificates.AsEnumerable().OrderBy(s => s.eDocsRef).ToList();
+                        break;
+                    case "status":
+                        model.lCertificates = model.lCertificates.AsEnumerable().OrderBy(s => s.status).ToList();
+                        break;
+                    case "datetime":
+                        model.lCertificates = model.lCertificates.AsEnumerable().OrderBy(s => s.datetime).ToList();
+                        break;
+                    case "cause":
+                        model.lCertificates = model.lCertificates.AsEnumerable().OrderBy(s => s.cause).ToList();
+                        break;
+                    case "postcode_desc":
+                        model.lCertificates = model.lCertificates.AsEnumerable().OrderByDescending(s => s.postcode).ToList(); ;
+                        break;
+                    case "operativename_desc":
+                        model.lCertificates = model.lCertificates.AsEnumerable().OrderByDescending(s => s.operativename).ToList();
+                        break;
+                    case "tradegroup_desc":
+                        model.lCertificates = model.lCertificates.AsEnumerable().OrderByDescending(s => s.tradegroup).ToList();
+                        break;
+                    case "eDocsRef_desc":
+                        model.lCertificates = model.lCertificates.AsEnumerable().OrderByDescending(s => s.eDocsRef).ToList();
+                        break;
+                    case "status_desc":
+                        model.lCertificates = model.lCertificates.AsEnumerable().OrderByDescending(s => s.status).ToList();
+                        break;
+                    case "datetime_desc":
+                        model.lCertificates = model.lCertificates.AsEnumerable().OrderByDescending(s => s.datetime).ToList();
+                        break;
+                    case "cause_desc":
+                        model.lCertificates = model.lCertificates.AsEnumerable().OrderByDescending(s => s.cause).ToList();
+                        break;
+                    default:
+                        model.lCertificates = model.lCertificates.AsEnumerable().OrderBy(s => s.propertyid).ToList();
+                        break;
+                }
+            }
+            return View(model);
         }
+
+
 
         // GET: Certificates/Details/5
         public ActionResult Details(int? id)
--- FirstTouchDashBoard/Models/ExtendedCertificates.cs
+++ FirstTouchDashBoard/Models/ExtendedCertificates.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,12 +9,23 @@ namespace FirstTouchDashBoard.Models
 {
     public class ExtendedCertificates
     {
+        [Required]
         public List<FirstTouchCertificate> lCertificates { get; set; }
         public DateTime from { get; set; }
         public DateTime to { get; set; }
         public string certType { get; set; }
-        public string uprn;
-        public string propertyid;
-        public string postCode;
+        public string uprn { get; set; }
+        public string propertyid { get; set; }
+        public string postCode { get; set; }
+
+        public ExtendedCertificates()
+        {
+            from = new DateTime(2000, 01, 01);
+            to = DateTime.Today;
+            certType = "both";
+            uprn = "";
+            postCode = "";
+            propertyid = "";
+        }
     }
 }
